50442d4fb268847f50850e23eea3c711a6fd0a98
Igor <igorvlasov732@gmail.com>
2024-09-14 19:02:26 +0400

    IdentifyFunction

---
+++ src/test/java/functions/IdentityFunctionTest.java
@@ -0,0 +1,28 @@
+package functions;
+import functions.IdentityFunction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IdentityFunctionTest
+{
+    private IdentityFunction func;
+    @BeforeEach
+    void createFunc()
+    {
+         func = new IdentityFunction();
+    }
+    @Test
+    void applyTestInt()
+    {
+        double res = func.apply(2);
+        Assertions.assertEquals(2,res);
+    }
+    @Test
+    void applyTestDouble()
+    {
+        double res = func.apply(-10.3);
+        Assertions.assertEquals(-10.3,res);
+    }
+
+}
