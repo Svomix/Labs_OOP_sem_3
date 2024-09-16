39c841a2cd044d89eb70c245dfeed659b24b0751
Igor <igorvlasov732@gmail.com>
2024-09-16 19:24:48 +0400

    Task3

---
+++ Lab1/Task3/MyFirstProgram.java
@@ -0,0 +1,43 @@
+class MyFirstClass {
+ 	public static void main(String[] s) {
+		MySecondClass o = new MySecondClass(4,2);
+		System.out.println(o.divide());
+		for (int i = 1; i <= 8; i++) {
+ 			for (int j = 1; j <= 8; j++) {
+     			o.setA(i);
+     			o.setB(j);
+     			System.out.print(o.divide());
+     			System.out.print(" ");
+ 	}
+ 	System.out.println();
+}
+
+ 	}
+}
+class MySecondClass{
+	private int a;
+	private int b;
+	public MySecondClass(final int a, final int b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	public int getA()
+	{
+		return a;
+	}
+	public int getB()
+	{
+		return b;
+	}
+	public void setA(final int a){
+	this.a = a;
+	}
+	public void setB(final int b){
+	this.b = b;
+	}
+	public int divide()
+	{
+		return a / b;
+	}
+}
