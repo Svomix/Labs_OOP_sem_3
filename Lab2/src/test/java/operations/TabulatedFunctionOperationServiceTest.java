3478446a62d9776ba6a82f51e2f8c4fb06ade8aa
split7 <pleshakov124@gmail.com>
2024-10-08 19:49:35 +0400

    Task2.1

--- Lab2/src/test/java/operations/TabulatedFunctionOperationServiceTest.java
+++ Lab2/src/test/java/operations/TabulatedFunctionOperationServiceTest.java
@@ -2,6 +2,8 @@ package operations;
 
 
 import functions.ArrayTabulatedFunction;
+import functions.LinkedListTabulatedFunction;
+import functions.Point;
 import functions.TabulatedFunction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,7 +12,15 @@ class TabulatedFunctionOperationServiceTest {
     @Test
     void test() {
         TabulatedFunction function = new ArrayTabulatedFunction(new double[]{1, 2, 3, 4, 5}, new double[]{1, 2, 3, 4, 5});
+        Point[] points = new Point[]{new Point(1, 1), new Point(2, 2), new Point(3, 3), new Point(4, 4), new Point(5, 5)};
+        Assertions.assertArrayEquals(points, TabulatedFunctionOperationService.asPoint(function));
+    }
 
+    @Test
+    void test2() {
+        TabulatedFunction function = new LinkedListTabulatedFunction(new double[]{0, 1.2, 2.5, 6.7, 8.8}, new double[]{1, 2, 3, 4, 5});
+        Point[] points = new Point[]{new Point(0, 1), new Point(1.2, 2), new Point(2.5, 3), new Point(6.7, 4), new Point(8.8, 5)};
+        Assertions.assertArrayEquals(points, TabulatedFunctionOperationService.asPoint(function));
     }
 
 }
