2b539870de5bf27dc5ea929c999181a775b59974
Igor <igorvlasov732@gmail.com>
2024-12-01 19:05:53 +0400

    Start front

---
+++ Lab2/src/main/java/Labs_OOP_sem_3/ui/pages/Function.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Function extends Component {
+    render() {
+        return (
+            <div>
+                Hello, func!
+            </div>
+        );
+    }
+}
+
+export default Function;
