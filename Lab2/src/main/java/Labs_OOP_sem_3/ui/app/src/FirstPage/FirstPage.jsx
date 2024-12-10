054ca0becd118039d0b38eedd02b8ed2f1da58d2
split7 <pleshakov124@gmail.com>
2024-12-10 19:57:16 +0400

    labs 7

--- Lab2/src/main/java/Labs_OOP_sem_3/ui/app/src/FirstPage/FirstPage.jsx
+++ Lab2/src/main/java/Labs_OOP_sem_3/ui/app/src/FirstPage/FirstPage.jsx
@@ -1,94 +1,98 @@
-import Header from "./components/Header/Header.jsx";
 import Button from "./components/Button/Button.jsx";
 import {useState} from "react";
 
 export default function FirstPage() {
-    const [pointsCount, setPointsCount] = useState(0);
+    const [pointsCount, setPointsCount] = useState();
     const [hasError, setHasError] = useState(true)
     const [tableData, setTableData] = useState([])
 
-    function changePointsCount(event){
+    function changePointsCount(event) {
         setPointsCount(event.target.value)
         setHasError(event.target.value < 2)
     }
 
-    function createTable() {
+    function createTable(event) {
+        event.preventDefault();
         const data = []
         for (let i = 1; i <= pointsCount; i++) {
-            data.push({id : i, x: null, y: null})
+            data.push({id: i, x: null, y: null})
         }
         setTableData(data)
     }
+
     function handleInputChange(id, field, newValue) {
         setTableData(prevData =>
-        prevData.map(item =>
-        item.id === id ? {... item, [field]:newValue}:item))
-    }
-    function areAllCellsFilled() {
-        return tableData.every(item => item.x !== '' && item.y !== '');
+            prevData.map(item =>
+                item.id === id ? {...item, [field]: newValue} : item))
     }
 
-    function handleSecondButtonClick(event) {
-        event.preventDefault();
-        alert('Все ячейки заполнены!');
+    function areAllCellsFilled() {
+        return tableData.every(item => item.x !== 0 && item.y !== 0);
+    }
+
+    function handleSecondButtonClick() {
+        if (areAllCellsFilled()) {
+            alert('Все ячейки заполнены!');
+        } else {
+            alert('Не все ячейки заполнены')
+        }
     }
 
     return (
         <div>
-            <Header/>
-            <main>
-                <section>
-                    <p3>Создание табулированной функции</p3>
-                    <form onSubmit={createTable}>
-                        <label htmlFor="pointsCount">Количество точек:</label>
-                        <input type="number"
-                               className="control"
-                               value={pointsCount}
-                               style={{
-                                   border: !hasError ? null : '1px solid red'
-                               }}
-                               onChange={changePointsCount}/>
-                        <Button onClick={createTable} disabled={hasError} isActive={!hasError}>Создать</Button>
-                    </form>
-                    {
-                        <table>
-                            <thead>
-                            <tr>
-                                <th>X</th>
-                                <th>Y</th>
+            <section>
+                <p3>Создание табулированной функции</p3>
+                <form onSubmit={createTable}>
+                    <label htmlFor="pointsCount">Количество точек:</label>
+                    <input type="number"
+                           className="control"
+                           id={pointsCount}
+                           value={pointsCount}
+                           style={{
+                               border: !hasError ? null : '1px solid red'
+                           }}
+                           onChange={changePointsCount}/>
+                    <Button onClick={createTable} disabled={hasError} isActive={!hasError}>Создать</Button>
+                </form>
+                {
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>X</th>
+                            <th>Y</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {tableData.map(item => (
+                            <tr key={item.id}>
+                                <td>
+                                    <input
+                                        type="number"
+                                        value={item.x}
+                                        onChange={(e) => handleInputChange(item.id, 'x', e.target.value)}
+                                    />
+                                </td>
+                                <td>
+                                    <input
+                                        type="number"
+                                        value={item.y}
+                                        onChange={(e) => handleInputChange(item.id, 'y', e.target.value)}
+                                    />
+                                </td>
                             </tr>
-                            </thead>
-                            <tbody>
-                            {tableData.map(item => (
-                                <tr key={item.id}>
-                                    <td>
-                                        <input
-                                            type="number"
-                                            value={item.x}
-                                            onChange={(e) => handleInputChange(item.id, 'x', e.target.value)}
-                                        />
-                                    </td>
-                                    <td>
-                                        <input
-                                            type="number"
-                                            value={item.y}
-                                            onChange={(e) => handleInputChange(item.id, 'y', e.target.value)}
-                                        />
-                                    </td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    }
-                    <Button
-                        disabled={!areAllCellsFilled()}
-                        isActive={areAllCellsFilled()}
-                        onClick={handleSecondButtonClick}
-                    >
-                        Подтвердить все ячейки
-                    </Button>
-                </section>
-            </main>
+                        ))}
+                        </tbody>
+                    </table>
+                }
+                {!hasError && <Button
+                    disabled={!areAllCellsFilled()}
+                    isActive={areAllCellsFilled()}
+                    onClick={handleSecondButtonClick}
+                >
+                    Подтвердить все ячейки
+                </Button>
+                }
+            </section>
         </div>
     )
 }
